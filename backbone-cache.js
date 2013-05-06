3ec58385db74ee02509a07fce927a8b76d927ab3
Tai-Jin Lee <taijin@gmail.com>
2013-05-06 14:39:22 -0700

    fixing some stupid mistakes around setting cache

--- backbone-cache.js
+++ backbone-cache.js
@@ -1,7 +1,7 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module and set browser global
-    define(['underscore', 'jquery', 'backbone', 'jscache'], function (_, $, Backbone, JSCache) {
+    define(['underscore', 'jquery', 'backbone', 'jscache'], function(_, $, Backbone, JSCache) {
       return (root.Backbone = factory(_, $, Backbone, JSCache));
     });
   } else {
@@ -10,27 +10,36 @@
   }
 }(this, function(_, $, Backbone, JSCache) {
 
-  Backbone.cache = new JSCache();
+  Backbone.cache = new JSCache(-1, false, new JSCache.LocalStorageCacheStorage('BackboneCache'));
   Backbone.cache.get = Backbone.cache.getItem;
-  Backbone.cache.set = Backbone.cache.setItem;
+  Backbone.cache.set = function(key, value, expireSeconds) {
+    var expireMillis = expireSeconds ? expireSeconds * 1000 : 86400000 /* 1 day */;
+    var expirationAbsolute = new Date((new Date().getTime()) + expireMillis);
+    Backbone.cache.setItem(key, value, { expirationAbsolute: expirationAbsolute });
+  };
   Backbone.cache.dirty = Backbone.cache.removeItem;
 
   var backboneSync = Backbone.sync;
   Backbone.sync = function(method, model, options) {
     var url = _.result(model, 'url');
+    var expireSeconds = options.expireSeconds || null;
+    var prefill = options.prefill || false;
 
     if (method === 'read' && options.cache === true) {
       var cached = Backbone.cache.get(url);
       if (cached !== null) {
-        model.set(cached);
-        model.trigger('sync');
-        return; // return xhr?
+        model.set(cached, { parse: true });
+        var mockXhr = new $.Deferred();
+        model.trigger('sync', model, mockXhr, options);
+        if (!prefill) {
+          return mockXhr.resolve(model);
+        } // otherwise fall through to do
       }
     }
 
-    model.on('sync', function() {
-      if (method === 'create') {
-        Backbone.cache.set(url, model.attributes, {expirationAbsolute: new Date().options.expiration });
+    model.on('sync', function(model, response, options) {
+      if (method === 'read') {
+        Backbone.cache.set(url, response, expireSeconds);
       } else if (method === 'update' || method === 'patch' || method === 'delete') {
         Backbone.cache.dirty(url);
       }
@@ -38,5 +47,7 @@
 
     backboneSync.apply(this, arguments);
   };
+
+  return Backbone;
 
 }));
